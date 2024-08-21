e941ea255999ae0cedb8447c5b38f1f7080e71b9
Elliot727 <39097740+Elliot727@users.noreply.github.com>
2024-08-21 11:36:57 +0100

    Fixed Issues

--- src/app/api/hello/route.tsx
+++ src/app/api/hello/route.tsx
@@ -1,28 +1,28 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { generateSecurePassword } from '../pages';
-export async function POST(req: NextRequest) {
-  const { master, url, seed, prefix, size } = await req.json();
+// import { NextRequest, NextResponse } from 'next/server';
+// import { generateSecurePassword } from '../pages';
+// export async function POST(req: NextRequest) {
+//   const { master, url, seed, prefix, size } = await req.json();
 
-  if (!master || !url || !seed || !size) {
-    return NextResponse.json({ error: 'Invalid input. Please ensure all required fields are filled.' }, { status: 400 });
-  }
+//   if (!master || !url || !seed || !size) {
+//     return NextResponse.json({ error: 'Invalid input. Please ensure all required fields are filled.' }, { status: 400 });
+//   }
 
-  try {
-    const password = generateSecurePassword(master, url, seed, prefix, parseInt(size, 10));
-    return NextResponse.json({ password });
-  } catch (error) {
-    return NextResponse.json({ error: 'Error generating password' }, { status: 500 });
-  }
-}
+//   try {
+//     const password = generateSecurePassword(master, url, seed, prefix, parseInt(size, 10));
+//     return NextResponse.json({ password });
+//   } catch (error) {
+//     return NextResponse.json({ error: 'Error generating password' }, { status: 500 });
+//   }
+// }
 
-export async function GET(req: NextRequest) {
-  return NextResponse.json({ message: 'Method not allowed' }, { status: 405 });
-}
+// export async function GET(req: NextRequest) {
+//   return NextResponse.json({ message: 'Method not allowed' }, { status: 405 });
+// }
 
-export async function PUT(req: NextRequest) {
-  return NextResponse.json({ message: 'Method not allowed' }, { status: 405 });
-}
+// export async function PUT(req: NextRequest) {
+//   return NextResponse.json({ message: 'Method not allowed' }, { status: 405 });
+// }
 
-export async function DELETE(req: NextRequest) {
-  return NextResponse.json({ message: 'Method not allowed' }, { status: 405 });
-}
+// export async function DELETE(req: NextRequest) {
+//   return NextResponse.json({ message: 'Method not allowed' }, { status: 405 });
+// }
